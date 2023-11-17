77b86bb823df04e5b21079b7050d126e5da88ea8
Jahidul Islam <jahidul.islam.sec@gmail.com>
2023-11-17 14:01:09 +0600

    second commit 2

--- account/models.py
+++ account/models.py
@@ -7,13 +7,14 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    phone_number = PhoneNumberField(blank=True)
+    # phone_number = PhoneNumberField(blank=True)
     street_address = models.CharField(max_length=255, blank=True)
     district = models.CharField(max_length=255, blank=True)
     division = models.CharField(max_length=255, blank=True)
     image = models.ImageField(null=True, max_length=100000, default='', blank=True)
 
-    REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'phone_number', 'street_address', 'district', 'division', 'is_staff', 'image']
+    # REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'phone_number', 'street_address', 'district', 'division', 'is_staff', 'image']
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'street_address', 'district', 'division', 'is_staff', 'image']
 
     def __str__(self) -> str:
         if self.first_name and self.last_login:
